fdb509b920bdc6bb350bd3d3cdf7e4e068bbbb70
krunalpatoliya <krunal@grid-scape.com>
2019-05-01 12:58:01 +0530

    added hello world

--- src/main/java/com/testing/DemoUI/DemoUiApplication.java
+++ src/main/java/com/testing/DemoUI/DemoUiApplication.java
@@ -11,6 +11,7 @@ public class DemoUiApplication extends SpringBootServletInitializer{
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		System.out.println("hi....");
+		System.out.println("helloWorld!....");
 		return application.sources(DemoUiApplication.class);
 	}
 	public static void main(String[] args) {
